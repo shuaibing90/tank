43e2506f99c22d95ea08cd88f9bd25b36d1b66d6
Fedeline <shuaibing90@qq.com>
2020-12-04 21:15:35 +0800

    加入配置文件让程序更灵活

--- src/tank/Main.java
+++ src/tank/Main.java
@@ -8,9 +8,10 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
+        int initCountTank =Integer.parseInt((String)PropertyMgr.get("initCountTank")) ;
 
         //初始化地方坦克
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < initCountTank; i++) {
             tankFrame.tankList.add(new Tank(50+i*80,200, Dir.DOWN,Group.BAD,tankFrame));
         }
 
---
+++ src/tank/PropertyMgr.java
@@ -0,0 +1,33 @@
+package tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @Description TODO
+ * @Author Fedeline
+ * @Date 2020/12/4 下午8:33
+ * 配置文件管理类
+ */
+public class PropertyMgr {
+    static Properties props = new Properties();
+
+
+    static {
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static Object get(String key){
+        if (props == null){
+            return null;
+        }
+        return props.get(key);
+
+    }
+
+
+}
